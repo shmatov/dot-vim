b58bb00bc8a9d67ba3f91f9b5ca0584693c2db1d
Roman Shmatov <shmatov@yandex-team.ru>
2014-07-02 21:46:34 +0600

    update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-1. Run: `bash ~/.vim/install.sh`
+0. Clone: `git clone https://github.com/shmatov/dot-vim.git ~/.vim`
+1. Install: `bash ~/.vim/install.sh`
 2. Enjoy! :)
--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
+
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "source ~/.vim/vimrc" >> ~/.vimrc
 vim +PluginInstall +qall
 
 # Patching
