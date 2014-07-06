a741ecf548f34db025d5743f5475ec758cde69a8
Roman Shmatov <shmatov@yandex-team.ru>
2014-07-06 20:50:55 +0600

    compile ycm

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ mkdir ~/.vim/undodir
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "source ~/.vim/vimrc" >> ~/.vimrc
 vim +PluginInstall +qall
+
+cd ~/.vim/bundle/YouCompleteMe
+./install.sh
