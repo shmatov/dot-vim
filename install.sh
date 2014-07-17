adc31f937005ea4fab70e49295750b8e9ca18cfa
Roman Shmatov <shmatov@yandex-team.ru>
2014-07-17 17:48:32 +0600

    disable ycm

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "source ~/.vim/vimrc" >> ~/.vimrc
 vim +PluginInstall +qall
 
-cd ~/.vim/bundle/YouCompleteMe
-./install.sh
+# cd ~/.vim/bundle/YouCompleteMe
+# ./install.sh
